eb2d55d8289b50601ee0d7405874ae8353bf9941
R Sánchez <rene.as.himself@gmail.com>
2020-06-02 09:53:26 -0500

    Markdown show notes

--- lib/reactor/content/podcast.ex
+++ lib/reactor/content/podcast.ex
@@ -16,7 +16,14 @@ defmodule Reactor.Content.Podcast do
   def changeset(podcast, attrs) do
     podcast
     |> cast(attrs, [:audio_url, :is_published, :notes_md, :subtitle, :title])
-    |> validate_required([:audio_url, :is_published, :notes_md, :subtitle, :title])
+    |> validate_required([:audio_url, :notes_md, :subtitle, :title])
     |> unique_constraint(:title)
+    |> gen_notes()
   end
+
+  defp gen_notes(%{valid?: true, changes: %{notes_md: text}} = changeset) do
+    put_change(changeset, :notes_html, AlchemistMarkdown.to_html(text))
+  end
+
+  defp gen_notes(changeset), do: changeset
 end
