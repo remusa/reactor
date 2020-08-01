97a3cf3a514355a9df23680b184439bf9b88c6d4
R Sánchez <rene.as.himself@gmail.com>
2020-08-01 15:38:20 -0500

    Passing redirect URLs + fragments

--- lib/reactor_web/controllers/comment_controller.ex
+++ lib/reactor_web/controllers/comment_controller.ex
@@ -4,6 +4,14 @@ defmodule ReactorWeb.CommentController do
   alias Reactor.Content
   alias Reactor.Content.Comment
 
+  plug(:authenticate_admin when not
+        (action in [:create, :show, :edit, :update, :delete))
+
+  plug(:logged_in_user when not (action in [:show]))
+  plug(:correct_user when not (action in [:edit, :update, :delete))
+
+  @default_route Routes.comment_path(ReactorWeb.Endpoint, :index)
+
   def index(conn, _params) do
     comments = Content.list_comments()
     render(conn, "index.html", comments: comments)
@@ -17,12 +25,31 @@ defmodule ReactorWeb.CommentController do
   def create(conn, %{"comment" => comment_params}) do
     case Content.create_comment(comment_params) do
       {:ok, comment} ->
+        url =
+          case params["redirect_url"] do
+            "#comment-new" ->
+              Routes.podcast_path(conn, :show, comment.podcast_id) <> "#comment-#{comment.id}"
+            nil ->
+              Routes.comment_path(conn, :show, comment)
+            x ->
+              x
+          end
+
         conn
         |> put_flash(:info, "Comment created successfully.")
-        |> redirect(to: Routes.comment_path(conn, :show, comment))
+        |> redirect(to: url)
 
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
+      {:error, %Ecto.Changeset{} = %{errors: [content: {msg, _}]}} ->
+        url =
+          case params["redirect_url"] do
+            "#comment-new" -> Routes.comment_path(conn, :index)
+            x -> x
+          end
+
+        conn
+        |> put_flash(:error, "Invalid comment: #{msg}")
+        |> redirect(to: url)
+        |> halt()
     end
   end
 
@@ -34,7 +61,8 @@ defmodule ReactorWeb.CommentController do
   def edit(conn, %{"id" => id}) do
     comment = Content.get_comment!(id)
     changeset = Content.change_comment(comment)
-    render(conn, "edit.html", comment: comment, changeset: changeset)
+
+    render(conn, "edit.html", comment: comment, changeset: changeset, redirect_url: redirect_url(params))
   end
 
   def update(conn, %{"id" => id, "comment" => comment_params}) do
@@ -44,7 +72,9 @@ defmodule ReactorWeb.CommentController do
       {:ok, comment} ->
         conn
         |> put_flash(:info, "Comment updated successfully.")
-        |> redirect(to: Routes.comment_path(conn, :show, comment))
+        |> redirect(
+            to: Routes.comment_path(conn, :show, comment, redirect_url: redirect_url(params))
+          )
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "edit.html", comment: comment, changeset: changeset)
@@ -58,5 +88,16 @@ defmodule ReactorWeb.CommentController do
     conn
     |> put_flash(:info, "Comment deleted successfully.")
     |> redirect(to: Routes.comment_path(conn, :index))
+  end
+
+  defp redirect_url(params, default \\ @default_route) do
+    case params["redirect_url"] do
+      nil -> default
+      redirect -> redirect
+    end
+  end
+
+  defp to_redirect_or(conn, default) do
+    redirect(conn, to: redirect_url(conn.params, default))
   end
 end
--- lib/reactor_web/views/comment_view.ex
+++ lib/reactor_web/views/comment_view.ex
@@ -1,3 +1,9 @@
 defmodule ReactorWeb.CommentView do
   use ReactorWeb, :view
+  alias ReactorWeb.Endpoint
+
+  def comment_url(comment = %Reactor.Content.Comment{}) do
+    Routes.podcast_path(Endpoint, :show, comment.podcast_id) <>
+      "#comment-#{comment.id}"
+  end
 end
